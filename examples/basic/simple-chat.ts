48360c01e748bca8f15e0df4016f17c291b8ca2a
Jason Roberts <dinkerer@gmail.com>
2025-11-03 20:21:47 +0200

    feat(0.4.3): cleanup example

--- examples/basic/simple-chat.ts
+++ examples/basic/simple-chat.ts
@@ -1,30 +1,17 @@
 import { Micro } from '../../src/index'
+import { microlog } from '../../src/utils'
 
 async function main() {
-  const client = new Micro({
-    // model: 'openai:gpt-4o-mini',
-    // model: 'openai:gpt-5-nano',
-    // model: 'openai:gpt-5-mini',
-    // model: 'openrouter:openai/gpt-oss-safeguard-20b', // reasoning .reasoning
-    model: 'openrouter:nvidia/nemotron-nano-12b-v2-vl:free', // reasoning .reasoning
-    // model: 'openrouter:minimax/minimax-m2:free', // reasoning .reasoning
-    // model: 'ai302:grok-4-fast-reasoning',
-    // model: 'ai302:sophnet/GLM-4.6', reasoning_content
-    // model: 'ai302:glm-4.5-air', // reasoning
-
-    // model: 'gemini:gemini-2.5-flash-lite', // reasoning <thought> tag
-    // model: 'deepseek:deepseek-reasoner',
-    debug: true,
-  }) // will always default to "openai:gpt-4.1-mini" as in Providers
+  const client = new Micro() // will always default to "openai:gpt-4.1-mini" as in Providers
 
   const response = await client.chat(
-    'Explain what a TypeScript library is in one sentence. Think hard.'
+    'Explain what a TypeScript library is in one sentence. Keep it simple.'
   )
 
-  // console.log('Assistant:', response.completion.content)
-  // console.log('\nModel:', response.metadata.model)
-  // console.log('Tokens:', response.metadata.tokensUsed?.total_tokens || 'N/A')
-  console.log('\nResponse:', JSON.stringify(response, null, 3))
+  microlog('Assistant:', response.completion.content)
+  microlog('Model:', response.metadata.model)
+  microlog('Tokens:', response.metadata.tokensUsed?.total_tokens || 'N/A')
+  microlog('Metadata:', response.metadata || 'N/A')
 }
 
 main().catch(console.error)
